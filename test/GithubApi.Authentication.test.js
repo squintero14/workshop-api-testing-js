f6f4b7e906a69aab2de4cb01cfc9a65497212756
Sebastian Quintero <jsebastianquintero@gmail.com>
2020-02-28 08:37:49 -0500

    authentication test super-agent

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -10,7 +10,7 @@ describe('Github Api Test', () => {
   describe('Authentication', () => {
     it('Via OAuth2 Tokens by Header', async () => {
       const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-        .auth('token', process.env.ACCESS_TOKEN)
+        .auth('token', process.env.ACCESS_TOKEN);
 
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
